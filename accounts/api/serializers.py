78c5d29bd3c38f64ce05543dd811a74fcca08ad6
Yufanzh <yfzhangpku@gmail.com>
2023-02-17 00:20:05 -0600

    add user registration function

--- accounts/api/serializers.py
+++ accounts/api/serializers.py
@@ -1,10 +1,43 @@
-from django.contrib.auth.models import User, Group 
-from rest_framework import serializers
+from django.contrib.auth.models import User
+from rest_framework import serializers, exceptions
 
-class UserSerializer(serializers.HyperlinkedModelSerializer):
+class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fileds = ('url', 'username', 'email')
+        fileds = ('username', 'email')
 
 
+class SignupSerializer(serializers.ModelSerializer):
+    username = serializers.CharField(max_length=20, min_length=6)
+    password = serializers.CharField(max_length=20, min_length=6)
+    email = serializers.EmailField()
 
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password')
+    
+    def validate(self, data):
+        if User.objects.filter(username=data['username'].lower()).exists():
+            raise exceptions.ValidationError({
+                'message':'This username has been used.'
+            })
+        
+        if User.objects.filter(email=data['email'].lower().exists()):
+            raise exceptions.ValidationError({
+                'message':'This email address already has an account.'
+            })
+        return data
+
+    
+    def create(self, validated_data):
+        username = validated_data['username'].lower()
+        email = validated_data['email'].lower()
+        password = validated_data['password']
+
+        user = User.objects.create_user(
+            username=username,
+            email=email,
+            password=password,
+        )
+
+        return user
--- accounts/api/views.py
+++ accounts/api/views.py
@@ -1,14 +1,48 @@
-from django.contrib.auth.models import User, Group 
+from accounts.api.serializers import UserSerializer
+from django.contrib.auth.models import User
 from rest_framework import viewsets
 from rest_framework import permissions
-from accounts.api.serializers import UserSerializer
+from rest_framework.response import Response
+from rest_framework.decorators import action
+from rest_framework.permissions import AllowAny
+from django.contrib.auth import (
+    authenticate as django_authenticate,
+    login as django_login,
+    logout as django_logout,
+)
+from accounts.api.serializers import SignupSerializer, LoginSerializer
 
 class UserViewSet(viewsets.ModelViewSet):
     """
     API endpoint that allows users to be viewed or edited.
     """
     queryset = User.objects.all().order_by('-date_joined')
-    serializer_class = UserSerializer()
-    permission_classes = [permissions.IsAuthenticated]
+    serializer_class = UserSerializer
+    permission_classes = (permissions.IsAuthenticated,)
 
+class AccountViewSet(viewsets.ViewSet):
+    permission_classes = (AllowAny, )
+    serializer_class = SignupSerializer
+
+    @action(methods=['POST'], detail=False)
+    def signup(self, request):
+        """
+        utilize username, email, password
+        for registration process
+        """
+
+        serializer = SignupSerializer(data=request.data)
+        if not serializer.is_valid():
+            return Response({
+                'success': False,
+                'message': "Please check input",
+                'errors': serializer.errors,
+            }, status=404)
+        
+        user = serializer.save()
+        django_login(request, user)
+        return Response({
+            'success': True,
+            'user': UserSerializer(user).data
+        })
 # Create your views here.
