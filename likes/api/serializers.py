987862af4e569d180bf6fff120896dca0133deb3
Yufanzh <yfzhangpku@gmail.com>
2023-03-23 00:34:02 -0500

    create api for likes

---
+++ likes/api/serializers.py
@@ -0,0 +1,48 @@
+from accounts.api.serializers import UserSerializer
+from comments.models import Comment
+from django.contrib.contenttypes.models import ContentType
+from likes.models import Likes
+from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
+from tweets.models import Tweet
+
+class LikeSerializer(serializers.ModelSerializer):
+    user = UserSerializer()
+
+    class Meta:
+        model = Likes
+        fields = ('user', 'created_at')
+
+class LikeSerializerForCreate(serializers.ModelSerializer):
+    content_type = serializers.ChoiceField(choices=['comment', 'tweet'])
+    object_id = serializers.IntegerField()
+
+    class Meta:
+        model = Likes
+        fields = ('content_type', 'object_id')
+
+    def _get_model_class(self, data):
+        if data['content_type'] == 'comment':
+            return Comment
+        if data['content_type'] == 'tweet':
+            return Tweet
+        return None
+    
+    def validate(self, data):
+        model_class = self._get_model_class(data)
+        if model_class is None:
+            raise ValidationError({'content_type': 'Content type does not exist'})
+        liked_object = model_class.objects.filter(id=data['object_id']).first()
+        if liked_object is None:
+            raise ValidationError({'object_id': 'Object does not exist'})
+        return data
+
+    def create(self, validated_data):
+        model_class = self._get_model_class(validated_data)
+        instance, _ = Likes.objects.get_or_create(
+            content_type = ContentType.objects.get_for_model(model_class),
+            object_id = validated_data['object_id'],
+            user=self.context['request'].user,
+        )
+
+        return instance
---
+++ likes/api/tests.py
@@ -0,0 +1,91 @@
+from testing.testcases import TestCase
+from rest_framework.test import APIClient
+
+LIKE_BASE_URL = '/api/likes/'
+
+class LikeApiTests(TestCase):
+
+    def setUp(self):
+        self.linghu, self.linghu_client = self.create_user_and_client('linghu')
+        self.dongxie, self.dongxie_client = self.create_user_and_client('dongxie')
+        self.anonymous_client = APIClient()
+    
+    def test_tweet_likes(self):
+        tweet = self.create_tweet(self.linghu)
+        data = {'content_type': 'tweet', 'object_id': tweet.id}
+
+        # anonymous is not allowed
+        response = self.anonymous_client.post(LIKE_BASE_URL, data)
+        self.assertEqual(response.status_code, 403)
+
+        # get is not allowed
+        response = self.linghu_client.get(LIKE_BASE_URL, data)
+        self.assertEqual(response.status_code, 405)
+
+        # wrong content_type
+        response = self.linghu_client.post(LIKE_BASE_URL, {
+            'content_type': 'twitter',
+            'object_id': tweet.id,
+        })
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual('content_type' in response.data['errors'], True)
+
+        # wrong object id
+        response = self.linghu_client.post(LIKE_BASE_URL, {
+            'content_type': 'tweet',
+            'object_id': -1,
+        })
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual('object_id' in response.data['errors'], True)
+
+        # post success
+        response = self.linghu_client.post(LIKE_BASE_URL, data)
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual(tweet.like_set.count(), 1)
+
+        # duplicate likes
+        self.linghu_client.post(LIKE_BASE_URL, data)
+        self.assertEqual(tweet.like_set.count(), 1)
+        self.dongxie_client.post(LIKE_BASE_URL, data)
+        self.assertEqual(tweet.like_set.count(), 2)
+
+    
+    def test_comment_likes(self):
+        tweet = self.create_tweet(self.linghu)
+        comment = self.create_comment(self.dongxie, tweet)
+        data = {'content_type': 'comment', 'object_id': comment.id}
+
+        # anonymous is not allowed
+        response = self.anonymous_client.post(LIKE_BASE_URL, data)
+        self.assertEqual(response.status_code, 403)
+
+        # get is not allowed
+        response = self.linghu_client.get(LIKE_BASE_URL, data)
+        self.assertEqual(response.status_code, 405)
+
+        # wrong content_type
+        response = self.linghu_client.post(LIKE_BASE_URL, {
+            'content_type': 'coment',
+            'object_id': comment.id,
+        })
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual('content_type' in response.data['errors'], True)
+
+        # wrong object id
+        response = self.linghu_client.post(LIKE_BASE_URL, {
+            'content_type': 'comment',
+            'object_id': -1,
+        })
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual('object_id' in response.data['errors'], True)
+
+        # post success
+        response = self.linghu_client.post(LIKE_BASE_URL, data)
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual(comment.like_set.count(), 1)
+
+        # duplicate likes
+        self.linghu_client.post(LIKE_BASE_URL, data)
+        self.assertEqual(comment.like_set.count(), 1)
+        self.dongxie_client.post(LIKE_BASE_URL, data)
+        self.assertEqual(comment.like_set.count(), 2)
