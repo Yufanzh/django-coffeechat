9998ea656741849779434f3694fbeafb70ce87ee
Yufanzh <yfzhangpku@gmail.com>
2023-03-24 00:15:54 -0500

    create likes cancel api

--- likes/api/tests.py
+++ likes/api/tests.py
@@ -2,6 +2,7 @@ from testing.testcases import TestCase
 from rest_framework.test import APIClient
 
 LIKE_BASE_URL = '/api/likes/'
+LIKE_CANCEL_URL = '/api/likes/cancel/'
 
 class LikeApiTests(TestCase):
 
@@ -89,3 +90,63 @@ class LikeApiTests(TestCase):
         self.assertEqual(comment.like_set.count(), 1)
         self.dongxie_client.post(LIKE_BASE_URL, data)
         self.assertEqual(comment.like_set.count(), 2)
+    
+    def test_cancel(self):
+        tweet = self.create_tweet(self.linghu)
+        comment = self.create_comment(self.dongxie, tweet)
+        like_comment_data = {'content_type': 'comment', 'object_id': comment.id}
+        like_tweet_data = {'content_type': 'tweet', 'object_id': tweet.id}
+        self.linghu_client.post(LIKE_BASE_URL, like_comment_data)
+        self.dongxie_client.post(LIKE_BASE_URL, like_tweet_data)
+        self.assertEqual(tweet.like_set.count(), 1)
+        self.assertEqual(comment.like_set.count(), 1)
+
+        # login required
+        response = self.anonymous_client.post(LIKE_CANCEL_URL,  like_comment_data)
+        self.assertEqual(response.status_code, 403)
+
+        # get not allowed
+        response = self.linghu_client.get(LIKE_CANCEL_URL, like_comment_data)
+        self.assertEqual(response.status_code, 405)  
+
+        # wrong content_type      
+        response = self.linghu_client.post(LIKE_CANCEL_URL, {
+            'content_type': 'wrong',
+            'object_id': 1,
+        })
+        self.assertEqual(response.status_code, 400)
+
+        # wrong object id
+        response = self.linghu_client.post(LIKE_CANCEL_URL, {
+            'content_type': 'comment',
+            'object_id': -1,
+        })
+        self.assertEqual(response.status_code, 400)
+
+        # dongxie has not liked before, not a problem
+        response = self.dongxie_client.post(LIKE_CANCEL_URL, like_comment_data)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data['deleted'], False)
+        self.assertEqual(tweet.like_set.count(), 1)
+        self.assertEqual(comment.like_set.count(), 1)
+
+        # successfully canceled
+        response = self.linghu_client.post(LIKE_CANCEL_URL, like_comment_data)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data['deleted'], True)
+        self.assertEqual(tweet.like_set.count(), 1)
+        self.assertEqual(comment.like_set.count(), 0)
+
+        # linghu has not liked before, not a problem
+        response = self.linghu_client.post(LIKE_CANCEL_URL, like_tweet_data)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data['deleted'], False)
+        self.assertEqual(tweet.like_set.count(), 1)
+        self.assertEqual(comment.like_set.count(), 0)
+
+        # dongxie's like has been canceled
+        response = self.dongxie_client.post(LIKE_CANCEL_URL, like_tweet_data)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data['deleted'], True)
+        self.assertEqual(tweet.like_set.count(), 0)
+        self.assertEqual(comment.like_set.count(), 0)       
