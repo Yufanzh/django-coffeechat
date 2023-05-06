70b0ebd51902b4ce4eb6ca44c1e2bb39dfa82cbc
Yufanzh <yfzhangpku@gmail.com>
2023-05-05 23:20:53 -0500

    fix pymysql to add celery for async tasks

--- coffeechat/celery.py
+++ coffeechat/celery.py
@@ -1,6 +1,9 @@
 import os
 
 from celery import Celery
+import pymysql
+
+pymysql.install_as_MySQLdb()
 
 # set the default Django settings module for the 'celery' program
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'coffeechat.settings')
